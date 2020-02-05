fa82d5623fc25b2f37e3063b300195c2947903df
kwanj <mwangikelvin23.com>
2020-02-05 18:10:52 +0300

    Updates

---
+++ precache-manifest.fb4c3c37ed0555deebde6563c67e009a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf0d00a6fefe0e08c923f313a2a0052b",
+    "url": "/about-me/index.html"
+  },
+  {
+    "revision": "7aa284ce28f7eacea95d",
+    "url": "/about-me/static/css/main.8fddce29.chunk.css"
+  },
+  {
+    "revision": "7b95587b360fcc5c1258",
+    "url": "/about-me/static/js/2.c5b3c12b.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/about-me/static/js/2.c5b3c12b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7aa284ce28f7eacea95d",
+    "url": "/about-me/static/js/main.49d0e4fd.chunk.js"
+  },
+  {
+    "revision": "12c1f65e7ebd5d9b1f89",
+    "url": "/about-me/static/js/runtime-main.2b12cd07.js"
+  },
+  {
+    "revision": "3573557b73ae19dcd863bae94047c259",
+    "url": "/about-me/static/media/portfolio.3573557b.jpg"
+  }
+]);
